b5891d3ca5442e350250e88dc1e7de7c2b3b09cc
Amodu Kehinde <amodukehindee@gmail.com>
2022-02-23 07:46:03 +0100

    first commit

---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+export const REAL_EMAIL = "amodukehindee@gmail.com";
+export const TEMP_EMAIL = "amodukehindee@gmail.com";
+export const RESUME_LINK =
+  "https://drive.google.com/file/d/1mP2P0CwjP4HIUg1f2N0FDFeoGsHrCu6e/view?usp=sharing";
---
+++ src/sections/index.ts
@@ -0,0 +1,7 @@
+export { HeroSection } from "./hero";
+export { WhoSection } from "./who";
+export { WorkSection } from "./work";
+export { ValuesSection } from "./values";
+export { SkillsSection } from "./skills";
+export { ShortSection } from "./short";
+export { FinalSection } from "./final";
